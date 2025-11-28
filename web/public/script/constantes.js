48244530cfe13f83ccc2ec51bc82c4cf7c9c30eb
lumpleme <108637505+lumpleme@users.noreply.github.com>
2025-11-28 04:18:07 +0000

    compilando para deploy

--- web/public/script/constantes.js
+++ web/public/script/constantes.js
@@ -1,5 +1,5 @@
 "use strict";
-const backendAddress = "https://animated-train-v9gpq6rv9gp26xjq-8000.app.github.dev/";
+const backendAddress = "https://livialuizaback.pythonanywhere.com/";
 //const backendAddress = "https://jubilant-engine-jv4g94x6qg43qqxx-8000.app.github.dev/"
 /** Authentication token */
 const tokenKeyword = 'Token ';
